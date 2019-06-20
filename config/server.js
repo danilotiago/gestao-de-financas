0cdf0125670abfe16937388b838789006499ac36
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-20 19:05:18 -0300

    adicionando depend. + model user

--- config/server.js
+++ config/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./cors')
 
 const server = express()
 const PORT = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(PORT, function() {
     console.log(`Backend is running on port ${PORT}.`)
