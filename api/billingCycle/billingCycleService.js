9034ffdce63041820a963904fc1a309e52fb1265
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-20 17:46:01 -0300

    padronizando mensagens de erro

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,4 +1,5 @@
 const billingCycle = require('./billingCycle')
+const _ = require('lodash')
 
 // define os metodos do crud com seus verbos
 billingCycle.methods(['get', 'post', 'put', 'delete'])
@@ -8,6 +9,27 @@ billingCycle.updateOptions({
     new: true,  // tras sempre o novo registro no update e nao o antigo como por default
     runValidators: true
 })
+
+billingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if (bundle.errors) {
+        const errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeRestfulErrors) {
+    const errors = []
+
+    _.forIn(nodeRestfulErrors, (error, param) => errors.push(error.message))
+
+    return errors
+}
 
 billingCycle.route('count', function(req, res, next) {
     billingCycle.countDocuments(function(error, value) {
--- config/database.js
+++ config/database.js
@@ -6,6 +6,6 @@ module.exports = mongoose.connect('mongodb://localhost/financeiro', {
 })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
-mongoose.Error.messages.Number.min = "O '{VALUE}' é menor que o valor mínimo de '{MIN}'"
-mongoose.Error.messages.Number.max = "O '{VALUE}' é maior que o valor máximo de '{MAX}'"
+mongoose.Error.messages.Number.min = "O '{VALUE}' é menor que o valor mínimo de '{MIN} para o atributo '{PATH}'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' é maior que o valor máximo de '{MAX} para o atributo '{PATH}'"
 mongoose.Error.messages.String.enum = "O '{VALUE}' não é valido para o atributo '{PATH}'"
