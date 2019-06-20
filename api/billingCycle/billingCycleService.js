b8a651e77362c740bd0e448ffe0136dc738ae3a5
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-20 13:34:24 -0300

    configurando opcoes para update

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,5 +1,12 @@
 const billingCycle = require('./billingCycle')
 
+// define os metodos do crud com seus verbos
 billingCycle.methods(['get', 'post', 'put', 'delete'])
+
+// configuracoes do metodo de update
+billingCycle.updateOptions({ 
+    new: true,  // tras sempre o novo registro no update e nao o antigo como por default
+    runValidators: true
+})
 
 module.exports = billingCycle
--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://localhost/financeiro', {useNewUrlParser: true })
+module.exports = mongoose.connect('mongodb://localhost/financeiro', {
+    useNewUrlParser: true,
+    useFindAndModify: false 
+})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' é menor que o valor mínimo de '{MIN}'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' é maior que o valor máximo de '{MAX}'"
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é valido para o atributo '{PATH}'"
