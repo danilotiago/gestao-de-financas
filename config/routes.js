379e9aa10fbdc384fd6481bb20f02411bbec568d
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-20 12:15:53 -0300

    registrando rotas do servico de ciclo de pagamentos

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const billingCycleService = require('../api/billingCycle/billingCycleService')
 
 module.exports = server => {
     
     const router = express.Router()
+
+    billingCycleService.register(router, '/billingCycles')
+
     server.use('/api', router)
 
 }
