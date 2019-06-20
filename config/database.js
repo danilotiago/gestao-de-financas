1b1e3c272957e5d6d7fd70660747140dd7ea2d69
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-20 12:35:06 -0300

    traduzindo mensagem de erro do required

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 
 module.exports = mongoose.connect('mongodb://localhost/financeiro', {useNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
